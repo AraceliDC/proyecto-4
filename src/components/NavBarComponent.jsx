9d5c4a3f364ddd53d69e01ce482b2689334032eb
AraceliDC <agdc.105@gmail.com>
2024-03-17 23:02:50 -0300

    img holder

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -2,8 +2,7 @@ import { Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../public/logo.png'
-
+import logo from '../assets/images/logo.png'
 import { Link } from 'react-router-dom'
 
 export const NavBarComponent = () => {
