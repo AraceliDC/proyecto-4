0e2f7ee0fe06c3c60bcae0d4ed5e6a938a2c30cd
AraceliDC <agdc.105@gmail.com>
2024-03-17 23:36:29 -0300

    card y footer

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 import holder from '../holder.json'
 
 export const Menu = () => {
@@ -8,8 +8,12 @@ export const Menu = () => {
       <h1>CONOCE NUESTROS PLATILLOS</h1>
       {
         holder.dishes.map(dish=> ( 
-        <Card key={dish.name}>
+        <Card key={dish.name} style={{width:'18rem'}}>
           <Card.Img variant='top' src={dish.image}/>
+          <Card.Body>
+            <Card.Title>{dish.name}</Card.Title>
+            <Button variant='warning'>Ingredientes</Button>
+          </Card.Body>
         </Card>))
       }
     </div>
