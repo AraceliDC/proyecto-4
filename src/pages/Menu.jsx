85d20d0a0e5cb28f5f3021367d8b3bb77746b4fa
AraceliDC <agdc.105@gmail.com>
2024-03-17 23:13:04 -0300

    card

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import holder from '../holder.json'
 
 export const Menu = () => {
   return (
     <div style={{display:'flex'}}>
       <h1>CONOCE NUESTROS PLATILLOS</h1>
-      <Card>
-        
-      </Card>
+      {
+        holder.dishes.map(dish=> ( 
+        <Card key={dish.name}>
+          <Card.Img variant='top' src={dish.image}/>
+        </Card>))
+      }
     </div>
   )
 }
