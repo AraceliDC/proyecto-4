5f87b30a129e19e57dbd68610b6d0bd6bd121e02
AraceliDC <agdc.105@gmail.com>
2024-03-18 22:03:14 -0300

    texto arreglado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ export const Home = () => {
         <div style={{display:'flex', flex:'column', width:'100%', justifyContent:'space-evenly'}}>
           <div style={{justifyContent: 'center', width: '20rem', height:'40rem', display:'flex', flexDirection:'column'}} className='m-5'>
             <h1 className='mb-5'>Bienvenido a HAPPY RAMYON</h1>
-            <p>Somo un restaurant familiar que empezó el año 2012, después de un viaje a Corea del Sur donde quedamos encantados con su cultura y comida. Debido a esto decidimos inicuar  nuestro propio local compartiendo nuestra pasión por la comida</p>
-            <p>Te invitamos a disfrutar de nuestra sabrosa comida estilo coreano en nuestro local. <br />Ramyeon, bibimbap, bulgogi, tteokbokki, entre otros. <br /> Revisa nuestro Menú</p>
+            <p>Somos un restaurant familiar que empezó el año 2012, después de un viaje a Corea del Sur donde quedamos encantados con su cultura y comida. Debido a esto decidimos iniciar nuestro propio local compartiendo nuestra pasión por la comida con ustedes</p>
+            <p>Te invitamos a disfrutar de nuestra sabrosa comida estilo coreano en nuestro local. <br />Ramyeon, bibimbap, bulgogi, tteokbokki, entre otros. <br /> Revisa nuestro Menú y recuerda que siempre puedes llegar sin reserva al local o si quieres asegurarte un lugar, puedes ir a nuestra sección de reservas y ver la disponibilidad</p>
           </div>
          <div style={{height:'40rem', width:'40rem', display:'flex', alignItems:'center'}} className='m-5'> 
             <Image src= {imgCentral}
